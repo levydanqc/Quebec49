692384a5f146a309a7e89c029b0d79935e2cb197
Dan Levy <danlevy.ca@icloud.com>
2021-02-18 17:34:41 -0500

    Validation des fichiers initiaux

---
+++ Quebec49/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Quebec49
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
