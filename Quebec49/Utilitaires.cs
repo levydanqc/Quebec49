dd5527b0317472484909e67268ea11f9ab42c72f
Dan Levy <danlevy.ca@icloud.com>
2021-02-21 00:23:38 -0500

    Ajout de la classe Intervalle

---
+++ Quebec49/Utilitaires.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Utilitaires
+{
+    public static class Aleatoire
+    {
+        public static Random g_rndGenerateur = new Random();
+
+        public static int GenererNombre(int iBorneSuperieure)
+        {
+            return g_rndGenerateur.Next(iBorneSuperieure + 1);
+        }
+    }
+    public static class Interval
+    {
+        /// <summary>
+        /// Fonction évaluant un nombre dans l'intervalle [1, 49].
+        /// </summary>
+        /// <param name="x">Nomre devant être évalué.</param>
+        /// <param name="max">Borne maximale de l'interval.</param>
+        /// <param name="min">Borne minimale de l'interval.</param>
+        /// <returns>'True' si le nombre est dans l'intervalle,
+        /// 'False' autrement.</returns>
+        public static bool InRange(int x, int max = 49, int min = 1) => ((x - max) * (x - min) <= 0);
+    }
+}
